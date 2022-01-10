23a4b4d46418eeebc33606c889f602595f046519
BearNinja123 <tonanhhoaduc@gmail.com>
2022-01-10 18:58:33 -0500

    a

--- root.sh
+++ root.sh
@@ -3,7 +3,7 @@
 sudo apt update
 sudo apt upgrade
 
-sudo apt install -y alsa-utils cloc compton coreutils feh git i3 lm-sensors man-db mpv mupdf ncurses neofetch network-manager nmap picom pulseaudio python3 python3-pip rxvt-unicode sox tmux unzip vim xserver-xorg-core xinit x11-xserver-utils zip
+sudo apt install -y alsa-utils cloc compton coreutils feh git i3 lm-sensors man-db mpv mupdf ncurses-base neofetch network-manager nmap picom pulseaudio python3 python3-pip rxvt-unicode sox tmux unzip vim xserver-xorg-core xinit x11-xserver-utils zip
 
 sudo apt install apt-transport-https curl
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
@@ -12,3 +12,6 @@ curl -s https://install.speedtest.net/app/cli/install.deb.sh | sudo bash
 sudo apt update
 sudo apt install brave-browser
 sudo apt install speedtest
+
+sudo curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o /usr/local/bin/youtube-dl
+sudo chmod a+rx /usr/local/bin/youtube-dl
